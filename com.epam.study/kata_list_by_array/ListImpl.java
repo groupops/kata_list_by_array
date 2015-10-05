12a36572d4d110ef93f8132cea7e40e53ac5fb43
sandyukraine <Oleksandr_Kohut@epam.com>
2015-10-05 09:39:45 +0200

    remove method: return null if there are no elements in array

--- com.epam.study/kata_list_by_array/ListImpl.java
+++ com.epam.study/kata_list_by_array/ListImpl.java
@@ -27,25 +27,22 @@ public class ListImpl<T> implements CustomList<T> {
 	}
 	
 	public T remove(int index) {
-		checkNumberOfElements(index);
-		T elementToRemove = listByArray[index];
-		
-		if (index != numberOfElements) {
-            System.arraycopy(listByArray, index + 1, listByArray, index, numberOfElements - index - 1);
-        }
-		
-		listByArray[numberOfElements - 1] = null;
-		
-		numberOfElements--;
-		return elementToRemove;
+		if (numberOfElements > 0) {
+			T elementToRemove = listByArray[index];
+			
+			if (index != numberOfElements) {
+				System.arraycopy(listByArray, index + 1, listByArray, index, numberOfElements - index - 1);
+			}
+			
+			listByArray[numberOfElements - 1] = null;
+			
+			numberOfElements--;
+			return elementToRemove;
+		} else {
+			return null;
+		}
 	}
 	
-	private void checkNumberOfElements(int index) {
-		if (index > numberOfElements - 1) {
-            throw new IndexOutOfBoundsException("No such element" + index);
-        }
-	}
-
 	public boolean contains(Object item) {
 		for (int i = 0; i < listByArray.length; i++) {
 			if (listByArray[i].equals(item)) {
